e7d44b5e25eb142de6331705717d272c70f38741
miya <fangqishenderen@vip.qq.com>
2018-08-09 16:54:47 +0800

    bf

--- config/fileinput.php
+++ config/fileinput.php
@@ -1,4 +1,12 @@
 <?php
 return [
     'default_language' => 'zh',
+    'route' => [
+        'group' => [
+            'middleware' => [],
+            'namespace' => 'App\Http\Controllers',
+        ],
+        'action' => '\App\Http\Controllers\FileinputController@upload',
+        'name' => 'fileinput.name',
+    ],
 ];
--- src/LaravelFileinputServiceProvider.php
+++ src/LaravelFileinputServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelFileinputServiceProvider extends ServiceProvider
 {
+    //延时加载
+    protected $defer = true;
+
     /**
      * Bootstrap the application services.
      *
@@ -18,6 +21,7 @@ class LaravelFileinputServiceProvider extends ServiceProvider
 
         $this->publishes([
             realpath(__DIR__ . '/../vendor/kartik-v/bootstrap-fileinput/') => public_path('vendor/laravel-fileinput'),
+            realpath(__DIR__ . '/../config/') => config_path(),
         ], 'laravel-fileinput');
 
         //定义多语言
@@ -27,10 +31,21 @@ class LaravelFileinputServiceProvider extends ServiceProvider
         $filePath = public_path() . $file;
 
         if (!\File::exists($filePath)) {
-            //Default is zh-cn
+            //Default is zh
             $file = "/vendor/laravel-fileinput/js/locales/zh.js";
         }
         \View::share('LaravelFileinputLangFile', $file);
+
+
+        //定义路由
+        $router = app('router');
+        //need add auth
+        $group = config('fileinput.route.group', []);
+
+        $router->group($group, function ($router) {
+            $router->any('/laravel-fileinput/server', config('fileinput.route.action'))
+                ->name(config('fileinput.route.name'));
+        });
     }
 
     /**
@@ -44,5 +59,10 @@ class LaravelFileinputServiceProvider extends ServiceProvider
         $this->app->singleton('fileinput', function () {
             return new LaravelFileinput();
         });
+    }
+
+    public function provides()
+    {
+        return ['fileinput'];
     }
 }

0dbe375f8bdd74238e69682ebd8b8b21c7bb89f7
miya <fangqishenderen@vip.qq.com>
2018-08-09 13:51:37 +0800

    null

--- resources/views/fileinput.blade.php
+++
@@ -1,6 +0,0 @@
-{{--载入fileinput相关资源--}}
-<link href="{{ asset('laravel-fileinput/css/fileinput.css') }}" media="all" rel="stylesheet" type="text/css"/>
-<link href="{{ asset('laravel-fileinput/themes/explorer-fa/theme.css') }}" media="all" rel="stylesheet" type="text/css"/>
-<script src="{{ asset('laravel-fileinput/js/plugins/sortable.js') }}" type="text/javascript"></script>
-<script src="{{ asset('laravel-fileinput/js/fileinput.js') }}" type="text/javascript"></script>
-<script src="{{ asset($FileinputLangFile) }}" type="text/javascript"></script>
